fe9dc9c4ffacac363286566b81ba3e4659ba40e9
Snape888 <smoonnusay@gmail.com>
2024-03-22 15:27:08 +0000

    removed ethereum provider

--- src/routes/test/+page.svelte
+++ src/routes/test/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import { chainId, account } from '$lib/web3modal';
 	import Card from '../../components/Card.svelte';
-	import { EthereumProvider } from '@walletconnect/ethereum-provider';
+
 	import { onMount } from 'svelte';
 
 	import { ALCHEMY_NODE_KEY_POLYGON_MAINNET, ALCHEMY_NODE_KEY_ETHEREUM_MAINNET, PROJECT_ID_WALLETCONNECT, VITE_PROJECT_ID} from '$lib/boilerplate/js/env.ts';
@@ -12,21 +12,7 @@
   
 	onMount(async () => {
 	  _chainId = $chainId;
-	  const provider = await EthereumProvider.init({
-		projectId: VITE_PROJECT_ID,
-		metadata: {
-		  name: 'My Website',
-		  description: 'My Website Description',
-		  url: 'https://mywebsite.com',
-		  icons: ['https://avatars.githubusercontent.com/u/37784886']
-		},
-		showQrModal: true,
-		optionalChains: [1, 137, 2020],
-		rpcMap: {
-		  1: 'https://eth-mainnet.alchemyapi.io/v2/' + ALCHEMY_NODE_KEY_ETHEREUM_MAINNET,
-		  137: 'https://polygon-mainnet.g.alchemy.com/v2/' + ALCHEMY_NODE_KEY_POLYGON_MAINNET,
-		},
-	  });
+	  
 	});
   </script>
   
