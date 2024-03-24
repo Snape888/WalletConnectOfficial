6d353bc95b69841bc15bcb2cf0fec13f08ae08a7
Snape888 <smoonnusay@gmail.com>
2024-03-24 20:47:08 +0000

    env update

--- src/lib/boilerplate/js/env.ts
+++ src/lib/boilerplate/js/env.ts
@@ -1,10 +1,10 @@
-export const ALCHEMY_NODE_KEY_ETHEREUM_MAINNET = import.meta.env.VITE_ALCHEMY_NODE_KEY_ETHEREUM_MAINNET;
-export const ALCHEMY_NODE_KEY_POLYGON_MAINNET = import.meta.env.VITE_ALCHEMY_NODE_KEY_POLYGON_MAINNET;
+export const ALCHEMY_NODE_KEY_ETHEREUM_MAINNET = import.meta.env.VITE_ALCHEMY_NODE_ETHEREUM_MAINNET;
+export const ALCHEMY_NODE_KEY_POLYGON_MAINNET = import.meta.env.VITE_ALCHEMY_NODE_POLYGON_MAINNET;
 export const PROJECT_ID_WALLETCONNECT = import.meta.env.VITE_PROJECT_ID_WALLETCONNECT;
 export const VITE_PROJECT_ID = import.meta.env.VITE_PROJECT_ID;
 export const GAS_URL = import.meta.env.VITE_GASURL;
 export const sheetId = import.meta.env.VITE_SHEET_ID;
-export const apiKey = import.meta.env.VITE_API_KEY;
+export const apiKey = import.meta.env.VITE_API;
 export const range = import.meta.env.VITE_ADDY_LIST;
 
 
